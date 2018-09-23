65489c8db8ef2c37dc0f44c222536be9c3790550
DimaKoshelev <43210335+DimaKoshelev@users.noreply.github.com>
2018-09-23 20:13:36 +0300

    Add files via upload

---
+++ Безымянный3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main(){
+setlocale(LC_ALL,"RUSSIAN");
+cout<<"Забирай меня скорей, увози за сто морей ,"<<endl;
+cout<<"И целуй меня везде 18 мне уже."<<endl;
+cout<<"Забирай меня скорей, увози за сто морей ,"<<endl;
+cout<<"И целуй меня везде я ведь взрослая уже."<<endl;
+return 0;
+}
+
+
